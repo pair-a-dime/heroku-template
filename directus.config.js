8c4b89ec232688ff847794ea00cd3243cc3a5b93
Doug Estey <doug.estey@gmail.com>
2023-05-31 18:57:55 -0300

    Config fixes

--- directus.config.js
+++ directus.config.js
@@ -5,7 +5,12 @@ module.exports = {
   KEY: process.env.KEY,
   SECRET: process.env.SECRET,
   PORT: process.env.PORT,
-  PUBLIC_URL: process.env.PUBLIC_URL,
+  PUBLIC_URL: `http://0.0.0.0:${process.env.PORT}${process.env.PUBLIC_URL}`,
   DB_CLIENT: process.env.DB_CLIENT,
-  DB_CONNECTION_STRING: process.env.DATABASE_URL
+  DB_CONNECTION_STRING: process.env.DATABASE_URL,
+  CORS_ENABLED: process.env.CORS_ENABLED,
+  CORS_ORIGIN: process.env.CORS_ORIGIN,
+  NODE_ENV: process.env.NODE_ENV,
+  EXTENSIONS_PATH: "/directus/extensions",
+  STORAGE_LOCAL_ROOT: "/directus/uploads",
 };
