35a01f2c127827572664f9120eeaae942864ac53
Doug Estey <doug.estey@gmail.com>
2023-05-31 18:28:07 -0300

    Fix env

--- directus.config.js
+++ directus.config.js
@@ -1,15 +1,11 @@
 // Heroku to Directus mappings
 module.exports = {
-  DB_CONNECTION_STRING:
-    process.env.DB_CONNECTION_STRING || process.env.DATABASE_URL,
-  RATE_LIMITER_REDIS: process.env.RATE_LIMITER_REDIS || process.env.REDIS_URL,
-  CACHE_REDIS: process.env.CACHE_REDIS || process.env.REDIS_URL,
-  EMAIL_FROM: process.env.EMAIL_FROM || process.env.MAILGUN_SMTP_LOGIN,
-  EMAIL_SMTP_HOST:
-    process.env.EMAIL_SMTP_HOST || process.env.MAILGUN_SMTP_SERVER,
-  EMAIL_SMTP_PORT: process.env.EMAIL_SMTP_PORT || process.env.MAILGUN_SMTP_PORT,
-  EMAIL_SMTP_USER:
-    process.env.EMAIL_SMTP_USER || process.env.MAILGUN_SMTP_LOGIN,
-  EMAIL_SMTP_PASSWORD:
-    process.env.EMAIL_SMTP_PASSWORD || process.env.MAILGUN_SMTP_PASSWORD,
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+  KEY: process.env.KEY,
+  SECRET: process.env.SECRET,
+  PORT: process.env.PORT,
+  PUBLIC_URL: process.env.PUBLIC_URL,
+  DB_CLIENT: process.env.DB_CLIENT,
+  DB_CONNECTION_STRING: process.env.DATABASE_URL
 };
